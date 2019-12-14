990d6e367b51a54106d8dad6a021d039569652ba
youhui1214 <405852890@qq.com>
2019-12-14 21:10:40 +0800

    feat: 基本loader学习

--- src/search.js
+++ src/search.js
@@ -1,3 +1,22 @@
+'use strict';
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './search.less';
+import logo from './images/test.png';
 
-document.write('search page');
+class Search extends React.Component {
+
+    render() {
+        return <div className='search-text'>
+            search text fdgdgrty
+            <img src={logo} alt="logo"/>
+        </div>
+    }
+}
+
+ReactDOM.render(
+    <Search/>,
+    document.getElementById('root')
+);
+
---
+++ webpack.dev.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+
+
+module.exports = {
+    entry: {
+        index: './src/index.js',
+        search: './src/search.js'
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.less$/,
+                use: ['style-loader', 'css-loader', 'less-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 10240
+                        }
+                    }
+                ]
+            },
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    }
+};
